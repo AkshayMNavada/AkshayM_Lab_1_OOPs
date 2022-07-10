84e8e1f3efcbf6acef437f9443f6b8c7e1999017
AkshayMNavada <48427568+AkshayMNavada@users.noreply.github.com>
2022-07-10 16:49:12 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,23 +1,26 @@
 public class Main {
     public static void main(String[] args) {
-        Rectangle rectangle = new Rectangle("Green","rectangle",2,4);
-        System.out.println("Area of rectangle is " + rectangle.calculateArea());
-        System.out.println("Perimeter of rectangle is " + rectangle.calculatePerimeter());
-        rectangle.drawShape();
+        Circle circle = new Circle("Green",3);
+        System.out.println("Area of circle is : " + circle.calculateArea());
+        System.out.println("Perimeter of circle is : " + circle.calculatePerimeter());
+        circle.display();
+        System.out.println();
+
+        Triangle triangle = new Triangle("Yellow",4,5);
+        System.out.println("Area of triangle is : " + triangle.calculateArea());
+        System.out.println("Perimeter of triangle is : " + triangle.calculatePerimeter());
+        triangle.display();
+        System.out.println();
+
+        Rectangle rectangle = new Rectangle("Red",6,7);
+        System.out.println("Area of rectangle is : " + rectangle.calculateArea());
+        System.out.println("Perimeter of rectangle is : " + rectangle.calculatePerimeter());
         rectangle.display();
         System.out.println();
 
-        Square square = new Square("Yellow","square",5);
-        System.out.println("Area of square is " + square.calculateArea());
-        System.out.println("Perimeter of square is " + square.calculatePerimeter());
-        square.drawShape();
+        Square square = new Square("Blue",8);
+        System.out.println("Area of square is : " + square.calculateArea());
+        System.out.println("Perimeter of square is : " + square.calculatePerimeter());
         square.display();
-        System.out.println();
-
-        Circle circle = new Circle("Red","circle",6);
-        System.out.println("Area of circle is " + circle.calculateArea());
-        System.out.println("Perimeter of circle is " + circle.calculatePerimeter());
-        circle.drawShape();
-        circle.display();
     }
 }
